9d3a0f1c07874c0e739799ee6aa0cdf28985e71a
Sherick Eksteen <sherick@axantus.nl>
2024-12-28 20:29:41 +0100

    optimizations for deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,56 +1,63 @@
-import resolve from '@rollup/plugin-node-resolve';
+import { babel } from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
-import babel from '@rollup/plugin-babel';
+import { dts } from 'rollup-plugin-dts';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
-import dts from 'rollup-plugin-dts';
+import pkg from './package.json' assert { type: 'json' };
 
-export default [
+const extensions = ['.js', '.jsx', '.ts', '.tsx'];
+
+const config = [
   {   
     input: 'app/index.ts', // Your main entry point.
     output: [
       {
-        file: 'dist/index.cjs.js',
+        file: pkg.main,
         format: 'cjs',
-        exports: 'named',
+        sourcemap: true,
       },
       {
-        file: 'dist/index.esm.js',
+        file: pkg.module,
         format: 'esm',
+        sourcemap: true,
       },
     ],
     plugins: [
       peerDepsExternal(),
-      resolve({
-        extensions: ['.js', '.jsx', '.ts', '.tsx'], // Add support for TypeScript and JSX
-      }),
+      nodeResolve({ extensions }),
       commonjs(),
       typescript({
-        tsconfig: './tsconfig.json', // Ensure this points to your tsconfig file
+        tsconfig: './tsconfig.json',
+        declaration: true,
+        declarationDir: 'dist',
+        exclude: ['**/*.test.tsx', '**/*.test.ts', '**/*.stories.tsx'],
       }),
       babel({
+        extensions,
         babelHelpers: 'bundled',
         exclude: 'node_modules/**',
-        presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'], // Add React and TypeScript presets
-        extensions: ['.js', '.jsx', '.ts', '.tsx'], // Handle these extensions
       }),
       postcss({
-        extract: true, // Extract CSS into a separate file
-        minimize: true, // Minify the CSS
-        sourceMap: true,
+        minimize: true,
+        modules: true,
+        extract: false,
       }),
     ],
-    external: ['react', 'react-dom', 'next'],
+    external: [
+      ...Object.keys(pkg.dependencies || {}),
+      ...Object.keys(pkg.peerDependencies || {}),
+    ],
   },
   {
     input: 'app/index.ts',
-    output: {
-      file: 'dist/index.d.ts',
-      format: 'es',
-    },
+    output: [{ file: pkg.types, format: 'esm' }],
     plugins: [dts({
-      tsconfig: './tsconfig.build.dts.json',  // Pointing to the new config
+      tsconfig: './tsconfig.build.dts.json',
     })],
+    external: [/\.css$/],
   },
 ];
+
+export default config;
